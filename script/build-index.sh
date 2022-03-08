d7e65c1f12efc284968365f6b5e6a111e3f116a2
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-03-08 18:48:10 +0900

    first commit

--- script/build-index.sh
+++ script/build-index.sh
@@ -37,7 +37,23 @@ ex(){
     eval $1
 }
 
-ex "mkdir -p $outprefix"
+ex "mkdir -p $outprefix/gtf_original $outprefix/gtf_chrUCSC"
 ex "unpigz -c $genome > $outprefix/genome.fa"
-ex "unpigz -c $gtf > $outprefix/chr.gtf"
-ex "rsem-prepare-reference --star -p 24 --gtf $outprefix/chr.gtf $outprefix/genome.fa $outprefix/$build"
+ex "unpigz -c $gtf > $outprefix/gtf_original/chr.gtf"
+ex "convertchr_fromEns2UCSC.pl $outprefix/gtf_original/chr.gtf > $outprefix/gtf_chrUCSC/chr.gtf"
+
+for dir in gtf_original gtf_chrUCSC
+do
+    ex "extract_proteincoding.pl $outprefix/$dir/chr.gtf > $outprefix/$dir/chr.proteincoding.gtf"
+    for head in $outprefix/$dir/chr $outprefix/$dir/chr.proteincoding
+    do
+        ex "gtf2refFlat -g $head.gtf > $head.transcript.refFlat"
+        ex "gtf2refFlat -u -g $head.gtf > $head.gene.refFlat"
+        cat $head.gene.refFlat | awk 'BEGIN { OFS="\t" } {if($4=="+") {print $3, $5, $5, $1} else {print $3, $6, $6, $1} }' | uniq | grep -v chrom > $head.gene.TSS.bed
+        cat $head.transcript.refFlat | awk 'BEGIN { OFS="\t" } {if($4=="+") {print $3, $5, $5, $14} else {print $3, $6, $6, $14} }' | uniq | grep -v chrom > $head.transcript.TSS.bed
+        cat $head.gene.refFlat | awk 'BEGIN { OFS="\t" } {if($4=="+") {print $3, $6, $6, $1} else {print $3, $5, $5, $1} }' | uniq | grep -v chrom > $head.gene.TES.bed
+        cat $head.transcript.refFlat | awk 'BEGIN { OFS="\t" } {if($4=="+") {print $3, $5, $5, $14} else {print $3, $5, $5, $14} }' | uniq | grep -v chrom > $head.transcript.TES.bed
+    done
+done
+
+ex "rsem-prepare-reference --star -p 24 --gtf $outprefix/gtf_chrUCSC/chr.gtf $outprefix/genome.fa $outprefix/$build"
