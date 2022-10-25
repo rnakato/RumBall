099a9a884c0a4820027e9849ceed8f936a8409af
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-10-25 23:00:58 +0900

    add GO

--- Dockerfiles/RumBall/edgeR.sh
+++ Dockerfiles/RumBall/edgeR.sh
@@ -2,16 +2,17 @@
 cmdname=`basename $0`
 function usage()
 {
-    echo "$cmdname [Options] <inputfile> <num of reps> <groupname>" 1>&2
+    echo "$cmdname [Options] <inputfile> <num of reps> <groupname> <species>" 1>&2
     echo '   <inputfile>: prefix of input matrix file' 1>&2
     echo '   <Ddir>: directory of gene annotation files' 1>&2
     echo '   <num of reps>: number of replicates (quated by ":")' 1>&2
     echo '   <group name>: labels of two groups compared (quated by ":")' 1>&2
+    echo '   <species>: [Human|Mouse|Rat|Fly|Celegans]' 1>&2
     echo '   Options:' 1>&2
     echo '      -t <float>: FDR threshould (default: 0.05)' 1>&2
     echo '      -n <int>: number of genes for GO analysis (default: 500)' 1>&2
     echo "  Example:" 1>&2
-    echo "   $cmdname Matrix 2:2 WT:KD" 1>&2
+    echo "   $cmdname Matrix 2:2 WT:KD Human" 1>&2
 }
 
 p=0.05
@@ -29,7 +30,7 @@ do
 done
 shift $((OPTIND - 1))
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   usage
   exit 1
 fi
@@ -37,12 +38,32 @@ fi
 outname=$1
 n=$2
 gname=$3
-
+sp=$4
 n1=$(cut -d':' -f1 <<<${n})
 n2=$(cut -d':' -f2 <<<${n})
 
+if test $sp = "Human"; then
+    orgdb=org.Hs.eg.db
+    orggp=hsapiens
+elif test $sp = "Mouse"; then
+    orgdb=org.Mm.eg.db
+    orggp=mmusculus
+elif test $sp = "Rat"; then
+    orgdb=org.Rn.eg.db
+    orggp=rnorvegicus
+elif test $sp = "Fly"; then
+    orgdb=org.Dm.eg.db
+    orggp=dmelanogaster
+elif test $sp = "Celegans"; then
+     orgdb=org.Ce.eg.db
+     orggp=celegans
+else
+    echo "[Note] Species $sp is not included in [Human|Mouse|Rat|Fly|Celegans]. GO analysis will be skipped."
+fi
+
 Rdir=$(cd $(dirname $0) && pwd)
 R="Rscript $Rdir/edgeR.R"
+
 
 ex(){ echo $1; eval $1; }
 
@@ -73,5 +94,16 @@ for str in genes isoforms; do
 done
 
 for ty in DEGs upDEGs downDEGs; do
-    Rscript $Rdir/run_clusterProfiler.R -i=$outname.genes.$postfix.edgeR.$ty.tsv -n=$nGene_GO -o=$outname.genes.$postfix.edgeR.GO.clusterProfiler.$ty
+    if test "$orgdb" != ""; then
+        Rscript $Rdir/run_clusterProfiler.R \
+                -i=$outname.genes.$postfix.edgeR.$ty.tsv \
+                -n=$nGene_GO -o=$outname.genes.$postfix.edgeR.GO.clusterProfiler.$ty \
+                -n=$nGene_GO -orgdb=$orgdb
+    fi
+done
+
+if test "$orggp" != ""; then
+    head=$outname.genes.$postfix.edgeR
+    Rscript $Rdir/run_gprofiler2.R -i_up=$head.upDEGs.tsv -i_down=$head.downDEGs.tsv \
+            -n=$nGene_GO -org=$orggp -o=$head.GO.gProfiler2
 done
