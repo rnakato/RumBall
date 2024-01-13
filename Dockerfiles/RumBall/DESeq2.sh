4925c629444a28dc851f9fbb083676a8956b0a6f
Eijy Nagai <eijynagai@gmail.com>
2024-01-14 07:03:33 +0900

    fixed issues with -l, -s and /work/

--- Dockerfiles/RumBall/DESeq2.sh
+++ Dockerfiles/RumBall/DESeq2.sh
@@ -2,37 +2,36 @@
 cmdname=`basename $0`
 function usage()
 {
-    echo "$cmdname [Options] <inputfile> <num of reps> <groupname> <species>" 1>&2
+    echo '$cmdname [Options] <inputfile> <num of reps> <groupname> <species>' 1>&2
     echo '   <inputfile>: prefix of input matrix file' 1>&2
     echo '   <Ddir>: directory of gene annotation files' 1>&2
     echo '   <num of reps>: number of replicates (quated by ":")' 1>&2
     echo '   <group name>: labels of two groups compared (quated by ":")' 1>&2
     echo '   <species>: [Human|Mouse|Rat|Fly|Celegans]' 1>&2
     echo '   Options:' 1>&2
+    echo '      -l <float>: log2 fold change threshold (default: 1)' 1>&2
     echo '      -t <FDR>: FDR threshould for GO analysis (default: 0.05)' 1>&2
     echo '      -n <int>: number of genes for GO analysis (default: 500)' 1>&2
-    echo '      -l <float>: log2 fold change threshold (default: 1)' 1>&2
-    echo "   Example:" 1>&2
-    echo "      $cmdname star/Matrix 2:2 WT:KD Human" 1>&2
+    echo '   Example:' 1>&2
+    echo '      $cmdname star/Matrix 2:2 WT:KD Human' 1>&2
 }
 
+l=1.0
 t=0.05
 nGene_GO=500
-l=1.0
 
-while getopts "t:n:l:" option
+while getopts "l:t:n:" option
 do
     case ${option} in
+        l) l=${OPTARG};;
         t) t=${OPTARG};;
         n) nGene_GO=${OPTARG};;
-        l) l=${OPTARG};;
         *)
             usage
             exit 1
             ;;
     esac
 done
-#echo "t: $t, nGene_GO: $nGene_GO, l: $l"
 shift $((OPTIND - 1))
 if [ $# -ne 4 ]; then
   usage
@@ -42,6 +41,7 @@ fi
 # Determine if running in Docker/Singularity or a standard environment
 if [ -n "$SINGULARITY_CONTAINER" ]; then
     # Configure paths or settings specific to Singularity
+    echo 'Running using singularity'
 else
     # Default configuration
     FILE_BASE_PATH="/work/"
@@ -72,7 +72,7 @@ elif [ "$sp" = "Celegans" ]; then
     orgdb=org.Ce.eg.db
     orggp=celegans
 else
-    echo "[Note] Species $sp is not included in [Human|Mouse|Rat|Fly|Celegans]. GO analysis will be skipped."
+    echo '[Note] Species $sp is not included in [Human|Mouse|Rat|Fly|Celegans]. GO analysis will be skipped.'
     orgdb=""
 fi
 
@@ -86,8 +86,8 @@ ex(){
 }
 
 postfix=count
-ex "$R -i=$outname.genes.$postfix.txt -n=$n -gname=$gname -o=$outname.genes.$postfix -p=$p -nrowname=2 -ncolskip=1 -s=$sp -lfcthre=$l"
-ex "$R -i=$outname.isoforms.$postfix.txt -n=$n -gname=$gname -o=$outname.isoforms.$postfix -p=$p -nrowname=2 -ncolskip=1 -s=$sp -lfcthre=$l"
+ex "$R -i=$outname.genes.$postfix.txt -n=$n -gname=$gname -o=$outname.genes.$postfix -p=$t -nrowname=2 -ncolskip=1 -s=$sp -lfcthre=$l"
+ex "$R -i=$outname.isoforms.$postfix.txt -n=$n -gname=$gname -o=$outname.isoforms.$postfix -p=$t -nrowname=2 -ncolskip=1 -s=$sp -lfcthre=$l"
 
 for str in genes isoforms; do
     for ty in DEGs upDEGs downDEGs; do
