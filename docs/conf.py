d300a7a419b0863a139d6522d06b7754be91ae57
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2024-03-16 14:59:54 +0900

    Update Manual

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ html_logo = "_static/img/RumBall_logo.png"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
